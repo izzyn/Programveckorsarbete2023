6d9a7b68709badd522907bda5dad6c95adb927f4
Fedik <fredrik.taylor@elev.ga.lbs.se>
2023-01-12 14:52:33 +0100

    loading screen text and day night cycle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    [SerializeField]
+    public int dayLength = 60;
+    [SerializeField]
+    public int nightLength = 90;
+
+    public static bool isNight;
     public static item scrap = new item();
     public static item wood = new item();
+
 
 
     // Start is called before the first frame update
@@ -20,6 +27,21 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void MakeDay()
+    {
+        isNight = false;
+
+        Invoke("MakeNight", dayLength);
+    }
+
+    void MakeNight()
+    {
+        isNight = true;
+
+        Invoke("MakeDay", nightLength);
+    }
+
     public class item
     {
         int amount;
