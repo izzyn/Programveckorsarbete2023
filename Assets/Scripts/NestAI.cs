2c60b217f92f02e1f44965c7c58a9df21600c32e
Spizzy <spizzy47spizzy@gmail.com>
2023-01-13 17:30:11 +0100

    Integrated the A* plugin into the project and also made it work together with the terrain generation

---
+++ Assets/Scripts/NestAI.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class NestAI : MonoBehaviour
+{
+    List<GameObject> createdEnemies = new List<GameObject>();
+    int mapsize;
+    List<int> availableSpawns = new List<int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        int id = SimplifyVector(new Vector2(-50.5f, 0.5f));
+        Debug.Log(id);
+        Debug.Log(DeSimplifyVector(id));
+
+
+    }
+    
+
+    //NOTE: DO NOT CHANGE THIS CODE, I HOTFIXED THIS AT 00:00 AND IT MIGHT BREAK IF TOUCHED
+    public IEnumerator spawnEnemies(float cooldown, List<GameObject> enemyTypes)
+    {
+        TerrainGenerator terrainGenerator = GameObject.Find("TerrainGenerator").GetComponent<TerrainGenerator>();
+        mapsize = terrainGenerator.GetMapSize;
+        int positionSimple = SimplifyVector(gameObject.transform.position);
+        List<int> totalSpawnLocations = new List<int>();
+        for (int y = 0; y < 3; y++) //Gets all the total 
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                int tryLocation = ((y + (((gameObject.GetComponent<ObjectData>().id - (gameObject.GetComponent<ObjectData>().id % mapsize)) / mapsize)) - 1) * mapsize) + (x + (gameObject.GetComponent<ObjectData>().id % mapsize));
+                if ((x != 1 || y != 1) && tryLocation % mapsize != 0 && Mathf.Abs((x + (gameObject.GetComponent<ObjectData>().id % mapsize) - gameObject.GetComponent<ObjectData>().id % mapsize)) <= 3) //Makes it so the units can't spawn ontop of the nest
+                {
+                    totalSpawnLocations.Add(tryLocation);
+                }
+            }
+        }
+        availableSpawns = totalSpawnLocations.Where(x => !terrainGenerator.GetWaterLoggedTiles.Contains(x-1)).ToList(); //Removes all the waterlogged tiles from the available spawn locations
+        Debug.Log(availableSpawns.Count);
+
+        while (gameObject != null)
+        {
+            if(availableSpawns.Count > 0)
+            {
+                yield return new WaitForSeconds(cooldown);
+                GameObject selectedEnemy = enemyTypes[UnityEngine.Random.Range(0, enemyTypes.Count)];
+                Vector2 enemyPosition = DeSimplifyVector(availableSpawns[UnityEngine.Random.Range(0, availableSpawns.Count)]);
+                if (enemyPosition.x >= ((mapsize / 2f)*-1) + 0.5f && enemyPosition.x <= mapsize / 2 - 0.5f && enemyPosition.y >= ((mapsize / 2f) * -1) + 0.5f && enemyPosition.y <= mapsize / 2 - 0.5f)
+                {
+                    GameObject.Instantiate(selectedEnemy, new Vector3(enemyPosition.x, enemyPosition.y, enemyPosition.y * 0.1f), Quaternion.identity);
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    int SimplifyVector(Vector2 vector)
+    {
+        return (int)((vector.x + 0.5f) + (mapsize/2)) + mapsize * (int)(-vector.y + 0.5f + (mapsize/2));
+    }
+    Vector2 DeSimplifyVector(int simplifiedVector)
+    {
+        return new Vector2((((simplifiedVector % mapsize) - mapsize/2)) - 0.5f,-(mapsize/2) +0.5f + ((simplifiedVector - (simplifiedVector % mapsize))/mapsize));
+    }
+}
---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Unity.Mathematics;
+using System.Linq;
+using Pathfinding;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    [SerializeField, Range(10, 1000)]
+    int mapSize; //total map size
+    public int GetMapSize => mapSize;
+    [SerializeField]
+    Tilemap waterTileMap;
+    [SerializeField]
+    Tilemap tilemap; //referance tilemap
+    public Tilemap GetTileMap => tilemap;
+    [SerializeField]
+    Tile ground;
+    [SerializeField]
+    Tile water;
+    [SerializeField, Range(0.0f, 1.0f)]
+    float waterThreshold; //The threshold for the value so that the water can spawn (higher number = less water)
+    [SerializeField, Range(0f, 1f)]
+    float waterNoise; //dictates the amount of noise  (higher number = more differance) 
+    [SerializeField, Range(1, 10)]
+    int waterPointyness;
+    [SerializeField, Range(1, 10)]
+    int waterGenerationDepth;
+    [SerializeField, Range(0f, 100000)]
+    int seed; //Dictates everything except for offsets
+    [SerializeField, Range(0f, 1f)]
+    float treeNoise;
+    [SerializeField, Range(0f, 1f)]
+    float treeThreshold;
+    [SerializeField, Range(0f, 1f)]
+    float treeOffset;
+    [SerializeField, Range(0f, 1f)]
+    float nestDensity; //Is inversed
+    [SerializeField, Range(0, 100)]
+    int nestDistance;
+    [SerializeField, Range(0f, 1f)]
+    float nestAmount; //Is inversed
+    [SerializeField]
+    GameObject treePrefab;
+    [SerializeField]
+    GameObject treeGroup;
+    [SerializeField]
+    GameObject bushObject;
+    [SerializeField]
+    GameObject nestObject;
+    [SerializeField]
+    GameObject nestGroup;
+    [SerializeField, Range(0f, 1f)]
+    float bushRange;
+    [SerializeField, Range(0f, 1f)]
+    float bushNoise;
+    [SerializeField, Range(0f, 1f)]
+    float bushOffset;
+
+    List<int> occupiedTiles = new List<int>(); //1 dimensional list to index the occupied tiles (2 dimensions would look simpler but be more computationally expensive)
+    public List<int> GetOccupiedTiles => occupiedTiles; //Getter for the occupied tile grid
+    List<int> waterLoggedTiles = new List<int>();
+    public List<int> GetWaterLoggedTiles => waterLoggedTiles;
+    GridGraph gg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AstarData data = AstarPath.active.data;
+        Debug.Log(data);
+        // This creates a Grid Graph
+        gg = data.AddGraph(typeof(GridGraph)) as GridGraph;
+        GenerateTerrain(seed);
+        Physics.SyncTransforms();
+        StartCoroutine(waitGenerate());
+    }
+    IEnumerator waitGenerate()
+    {
+        yield return new WaitForSeconds(0.5f);
+        AstarPath.active.Scan();    
+    }
+    void GenerateTerrain(int presetSeed = 0)
+    {
+        // Setup a grid graph with some values
+        gg.center = new Vector3(0.5f, 0.5f, 0);
+        // Updates internal size from the above values
+        gg.SetDimensions(mapSize, mapSize, 1);
+        gg.is2D = true;
+        gg.collision.use2D = true;
+        gg.collision.mask += LayerMask.GetMask("Obstacle");
+        // Scans all graphs, do not call gg.Scan(), that is an internal method
+        int seed;
+        tilemap.ClearAllTiles(); //Resets the map
+        if(presetSeed < 100)
+        {
+            seed = UnityEngine.Random.Range(100, 100000); //Generates the seed value
+        }
+        else
+        {
+            seed = presetSeed;
+        }
+        for(int y = 0; y < mapSize; y++)
+        {
+            for(int x = 0; x < mapSize; x++)
+            {
+
+                float noiseNumber = NoiseFunctions.ridgedFractalNoise(x, y, seed, waterNoise, waterGenerationDepth, waterPointyness); //Generates a random perlin value for every cordinate
+                Vector3Int tilePosition = new Vector3Int((mapSize / 2) * -1 + x, (mapSize / 2) * -1 + y, 0); //Sets the position so that the map is centered on 0, 0
+                if(noiseNumber > waterThreshold && !(y < 5 + mapSize/2 && y > -5 + mapSize/2 && x < 5 + mapSize/2 && x > -5 + mapSize/2)) //Sets the water depending on the threshhold and makes sure there is an area for the player to spawn in
+                {
+                    waterTileMap.SetTile(tilePosition, water); //sets the tiles
+                    tilemap.SetTile(tilePosition, water); //sets the tiles
+                    waterLoggedTiles.Add(y * mapSize + x);
+                }
+                else
+                {
+                    tilemap.SetTile(tilePosition, ground);
+                }
+            }
+        }
+        int attempts = 0;
+        while(nestGroup.transform.childCount < 1 && attempts < 100)
+        {
+            GenerateObjects(treePrefab, treeNoise, treeOffset, treeThreshold, treeGroup.transform, seed, 0, 0.5f); //Generates the objects that we desire on the map
+            GenerateObjects(bushObject, bushNoise, bushOffset, bushRange, treeGroup.transform, seed, 0, 0.5f);
+            GenerateObjects(nestObject, nestDensity, 0, nestAmount, nestGroup.transform, seed, nestDistance, 0.5f);
+            if(nestGroup.transform.childCount < 1)
+            {
+                attempts++;
+                seed = UnityEngine.Random.Range(100, 100000); //Generates a new seed if the generation failed based on set conditions
+                Debug.Log("Generation failed, new seed is: " + seed);
+            }
+        }
+        if(attempts >= 100) //To prevent infinite loops, it automatically stops after 100 iterations
+        {
+            generationFail();
+        }
+        else
+        {
+            attempts = 0;
+            nestGroup.GetComponent<NestController>().startSpawning(); //Makes all nests start spawning enemies after the terrain has finished loading
+        }
+    }
+
+    void GenerateObjects(GameObject spawnObject, float noise, float offset, float threshold, Transform parentObject, int presetSeed = 0, float distance = 0, float constantOffset = 0)
+    {
+        int seed;
+        if (presetSeed < 100)
+        {
+            seed = UnityEngine.Random.Range(100, 100000); //Generates the seed value
+        }
+        else
+        {
+            seed = presetSeed;
+        }
+        for (int y = 0; y < mapSize; y++)
+        {
+            for (int x = 0; x < mapSize; x++) {
+                float offsetX = UnityEngine.Random.Range(-offset, offset); //Offsets the objects for aesthetic reasons
+                float offsetY = UnityEngine.Random.Range(-offset, offset);
+                float noiseNumber = Math.Clamp(Mathf.PerlinNoise(x * noise + seed*2, y * noise + seed*2), 0, 1); //Generates a random perlin value for every cordinate
+                Vector3Int tilePosition = new Vector3Int((mapSize / 2) * -1 + x, (mapSize / 2) * -1 + y, 0); //Sets the position so that the map is centered on 0, 0
+                //Sets the position, not sure about the random numbers, but it works.
+                Vector3 treePosition = new Vector3((mapSize / 2) * -1 + x, (mapSize / 2) * -1 + y + offsetY - constantOffset  + 0.5f, ((mapSize / 2) * -1 + y - offsetY - 0.5f)*0.1f);
+                Vector3Int positionRoundedDown = new Vector3Int((int)Math.Floor(treePosition.x), (int)Math.Floor(treePosition.y), 0); //Checks the adjacent tiles so trees don't spawn on water
+                Vector3Int positionRoundedUp = new Vector3Int((int)Math.Ceiling(treePosition.x), (int)Math.Ceiling(treePosition.y), 0);
+                Vector3Int XRoundedUp = new Vector3Int((int)Math.Ceiling(treePosition.x), (int)Math.Floor(treePosition.y), 0);
+                Vector3Int YRoundedUp = new Vector3Int((int)Math.Floor(treePosition.x), (int)Math.Ceiling(treePosition.y), 0);
+
+                float originDistance = Mathf.Sqrt(Mathf.Pow(Mathf.Abs(treePosition.x), 2) + Mathf.Pow(Mathf.Abs(treePosition.y), 2)); //Gets the distance from 0, 0 (doesn't need subtraction since the co-ordinates are 0, 0)
+
+                if (noiseNumber > threshold &&  !waterLoggedTiles.Contains(y * mapSize + x)  && !occupiedTiles.Contains(y*mapSize + x) && originDistance >= distance * 0.01f * (mapSize/2))
+                {
+                    GameObject placedTree = GameObject.Instantiate(spawnObject, treePosition, Quaternion.identity, parentObject);
+                    occupiedTiles.Add(y * mapSize + x); //Indexes the placed objects so it doesn't cause objects piling on the same tile
+                    placedTree.GetComponent<ObjectData>().id = y * mapSize + x;
+                    //This formula sets the position things that might destroy the water and object checks, do not touch
+                    placedTree.transform.position = new Vector3(placedTree.transform.position.x + 0.5f + offsetX, placedTree.transform.position.y + 0.5f + offsetY, (placedTree.transform.position.y + 0.5f + offsetY) * 0.1f);
+                }
+            }
+        }
+    }
+    void generationFail()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
