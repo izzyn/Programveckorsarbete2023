70aa4beddf612e53550a7dd64d7ad3ec9a190edd
madinsomiac <119925571+madinsomiac@users.noreply.github.com>
2023-01-16 11:51:53 +0100

    system

--- Assets/weaponSystem.cs
+++ Assets/weaponSystem.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class weaponSystem : MonoBehaviour
 {
-    KeyCode AttackKey
+    [SerializeField]
+    private int dmg_spear;
+    [SerializeField]
+    private int dmg_axe;
+    [SerializeField]
+    public Dictionary<ItemType, int> dmg = new Dictionary<ItemType, int>();
+    KeyCode AttackKey = KeyCode.Mouse0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dmg.Add(ItemType.Spear, dmg_spear);
+        dmg.Add(ItemType.Axe, dmg_axe);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(AttackKey))
+        {
+            Item item = Inventory.GetSelectedItem();
+            if(item.GetItemType()==ItemType.Spear)
+            {
+
+            }
+            if (item.GetItemType() == ItemType.Axe)
+            {
+
+            }
+        }
     }
 }
