5dfb8d4ce768fbc13bcc58a702e62d921ac69fba
Izzy <isak.attila@gmail.com>
2023-01-16 23:31:18 +0100

    Added working (but extremely laggy) pathfinding system

--- Assets/AIScript.cs
+++ Assets/AIScript.cs
@@ -8,12 +8,17 @@ public class AIScript : MonoBehaviour
     public int currentTile;
     int mapSize;
     List<int> obstacleTiles;
+    Vector2[] test = new Vector2[0];
+    Rigidbody2D rb;
+    int currentNode;
+    [SerializeField]
+    int speed;
     private void Start()
     {
         mapSize = GameObject.Find("TerrainGenerator").GetComponent<TerrainGenerator>().GetMapSize;
         obstacleTiles = GameObject.Find("TerrainGenerator").GetComponent<TerrainGenerator>().GetWaterLoggedTiles;
-        Vector2[] test = PathFinding();
-        Debug.Log(test);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        test = PathFinding();
 
     }
     //Does A*
@@ -21,6 +26,7 @@ public class AIScript : MonoBehaviour
     //The formula for Y is shortened due to integer rounding making the need to subtract the x formula redundant
     public Vector2[] PathFinding()
     {
+        float t1 = Time.realtimeSinceStartup;
         List<Vector2> path = new List<Vector2>();
         GameObject player = GameObject.FindWithTag("Player");
         int playerPosition = SimplifyVector((Vector2)player.transform.position);
@@ -36,9 +42,12 @@ public class AIScript : MonoBehaviour
         checkTiles.Add(SimplifyVector((Vector2)transform.position));
         int startTileX = checkTiles[0] % mapSize;
         int startTileY = checkTiles[0] / mapSize;
-        targetDistance[checkTiles[0]] = Mathf.Min(Mathf.Abs(playerX - startTileX), Mathf.Abs(playerY - startTileY)) * Mathf.Sqrt(2) + Mathf.Abs(playerX - startTileX - (playerY - startTileY));
+        //xctargetDistance[checkTiles[0]] = Mathf.Min(Mathf.Abs(playerX - startTileX), Mathf.Abs(playerY - startTileY)) * Mathf.Sqrt(2) + Mathf.Abs(playerX - startTileX - (playerY - startTileY));
+        targetDistance[checkTiles[0]] = new Vector2(playerX - startTileX, playerY - startTileY).magnitude;
+
         int currentTileIndex = 0;
         int backwardsSearchTile = playerPosition;
+        int debug = currentTileIndex;
 
         while (true)
         {
@@ -47,13 +56,14 @@ public class AIScript : MonoBehaviour
                 break;
             }
             List<int> walkableTiles = getWalkablePositions(checkTiles[currentTileIndex]);
+            int abc = 0;
                 for (int i = 0; i < walkableTiles.Count; i++)
                 {
                     checkTileX = walkableTiles[i] % mapSize;
                     checkTileY = walkableTiles[i] / mapSize;
                     //shortTargetDistance[i] = Mathf.Min(Mathf.Abs(playerX - checkTileX), Mathf.Abs(playerY - checkTileY)) * Mathf.Sqrt(2) + Mathf.Abs(playerX - checkTileX - (playerY - checkTileY));
                     Vector2 towardPlayerVector = new Vector2(playerX - checkTileX, playerY - checkTileY);
-                    shortTargetDistance[i] = towardPlayerVector.sqrMagnitude;
+                    shortTargetDistance[i] = towardPlayerVector.magnitude;
                     shortEnemyDistance[i] = enemyDistnace[checkTiles[currentTileIndex]] + Mathf.Sqrt(Mathf.Abs(checkTileX - (checkTiles[currentTileIndex] % mapSize)) + Mathf.Abs(checkTileY - (checkTiles[currentTileIndex] / mapSize)));
                     float sumDistance = shortEnemyDistance[i] + shortTargetDistance[i];
                     if (sumDistance < targetDistance[walkableTiles[i]] + enemyDistnace[walkableTiles[i]] || targetDistance[walkableTiles[i]] + enemyDistnace[walkableTiles[i]] == 0)
@@ -77,7 +87,7 @@ public class AIScript : MonoBehaviour
                     {
                         if (targetDistance[checkTiles[i]] + enemyDistnace[checkTiles[i]] == tempValue)
                         {
-                            if (tempTargetValue < targetDistance[checkTiles[i]] || tempIndex == -1)
+                            if (tempTargetValue < targetDistance[checkTiles[i]])
                             {
                                 tempValue = targetDistance[checkTiles[i]] + enemyDistnace[checkTiles[i]];
                                 tempTargetValue = targetDistance[checkTiles[i]];
@@ -94,34 +104,56 @@ public class AIScript : MonoBehaviour
                         }
                     }
                 }
+                /*
+                 
+                */
             currentTileIndex = tempIndex; 
         }
-        /*while (true)
+        float t2 = Time.realtimeSinceStartup;
+
+        while (true)
         {
             List<int> walkableTiles = getWalkablePositions(backwardsSearchTile);
             int tempIndex = 0;
             float tempValue = -1;
-
+            float tempSegLength = 0;
             for (int i = 0; i < walkableTiles.Count; i++)
             {
-                if (enemyDistnace[walkableTiles[i]] < tempValue || tempValue == -1 && (enemyDistnace[walkableTiles[i]] != 0 || walkableTiles[i] == SimplifyVector((Vector2)(gameObject.transform.position))))
+                float segLength = Mathf.Sqrt(Mathf.Abs(walkableTiles[i] % mapSize - backwardsSearchTile % mapSize) + Mathf.Abs(walkableTiles[i] / mapSize - backwardsSearchTile / mapSize));
+                float newValue = enemyDistnace[walkableTiles[i]] + segLength;
+                if ((newValue < tempValue || tempValue == -1 || (newValue - tempValue < 0.01f  && new Vector2(startTileX - (walkableTiles[i] % mapSize), startTileY - (walkableTiles[i] / mapSize)).magnitude + segLength < new Vector2(startTileX - (tempIndex % mapSize), startTileY - (tempIndex / mapSize)).magnitude + tempSegLength)) && (enemyDistnace[walkableTiles[i]] != 0 || walkableTiles[i] == SimplifyVector((Vector2)(gameObject.transform.position))))
                 {
                     tempIndex = walkableTiles[i];
-                    tempValue = enemyDistnace[walkableTiles[i]];
+                    tempValue = newValue;
+                    tempSegLength = segLength;
                 }
             }
-            path.Add(DeSimplifyVector(backwardsSearchTile) - DeSimplifyVector(tempIndex));
+            path.Add(DeSimplifyVector(tempIndex));
             if (tempIndex == SimplifyVector((Vector2)(gameObject.transform.position)))
             {
                 break;
             }
             backwardsSearchTile = tempIndex;
         }
-        */
-        
+        float t3 = Time.realtimeSinceStartup;
+
+
         path.Reverse();
         return path.ToArray();
 
+    }
+
+    private void FixedUpdate()
+    {
+        if(test.Length > 0)
+        {
+            rb.AddForce((test[currentNode] - (Vector2)transform.position).normalized * Time.deltaTime * speed);
+            rb.velocity *= Mathf.Pow(0.9f, Time.deltaTime);
+            if (Vector2.Distance(test[currentNode], (Vector2)transform.position) < 0.2f && currentNode < test.Length - 1)
+            {
+                currentNode++;
+            }
+        }
     }
     public List<int> getWalkablePositions(int tile)
     {
--- Assets/Scripts/NestAI.cs
+++ Assets/Scripts/NestAI.cs
@@ -13,7 +13,6 @@ public class NestAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(SimplifyVector((Vector2)transform.position));
 
     }
     
