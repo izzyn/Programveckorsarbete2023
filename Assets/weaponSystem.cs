80db55f6e0017955f832685f9df48638c485f729
madinsomiac <119925571+madinsomiac@users.noreply.github.com>
2023-01-18 14:17:52 +0100

    axebo

--- Assets/weaponSystem.cs
+++ Assets/weaponSystem.cs
@@ -16,7 +16,8 @@ public class weaponSystem : MonoBehaviour
     private int dmg_axe =1;
     [SerializeField]
     public Dictionary<ItemType, int> dmg = new Dictionary<ItemType, int>();
-
+    [SerializeField]
+    private int timer;
     KeyCode AttackKey = KeyCode.Mouse0;
     // Start is called before the first frame update
     void Start()
@@ -25,41 +26,49 @@ public class weaponSystem : MonoBehaviour
         dmg.Add(ItemType.Spear, dmg_spear);
         dmg.Add(ItemType.Axe, dmg_axe);
         anim = gameObject.GetComponent<Animator>();
-        Inventory.AddItem(new AxeItem());
+      //  Inventory.AddItem(new AxeItem());
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        timer= timer-1;
 
         if(Input.GetKeyDown(AttackKey))
         {
+            timer = 80;
             Item item = Inventory.GetSelectedItem();
-            if (item == null)
-            {
-                print("null return");
-                return; 
-            }
-            dmg.TryGetValue(item.GetItemType(), out CurrentDamage);
-            SpriteRenderer.sprite = ((ItemUsable)item).GetInUseSprite();
-            if (item.GetItemType()==ItemType.Spear)
+            //if (item == null)
+            //{
+               // print("null return");
+                //return; 
+            //}
+            dmg.TryGetValue(ItemType.Axe, out CurrentDamage);
+            SpriteRenderer.sprite = Resources.Load<Sprite>("Textures/Tools/Axe");
+            if (ItemType.Axe == ItemType.Spear)
             {
            
             }
-            if (item.GetItemType() == ItemType.Axe)
+            if (ItemType.Axe == ItemType.Axe)
             {
+
                 print("axe");
                 anim.SetBool("axe", true);   
             }
-            if(anim.GetBool("axe")==true)
+           if(timer <= 0)
             {
-                anim.SetBool("axe", true);
-                
+                anim.SetBool("axe", false);
             }
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void AlertObservers(string message)
+    {
+        if (message.Equals("axeAttack"))
+        {
+            anim.SetBool("axe", false);
+        }
+    }
+        private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Enemy"))
         {
@@ -72,12 +81,7 @@ public class weaponSystem : MonoBehaviour
         }
         
     }
-        public void AlertObservers(string message)
-    {
-        if(message.Equals("axeAttack"))
-        {
-            anim.SetBool("axe", false);
-        }
+        
     }
 
-}
+
